77b5553d1be0bcfeeb5f4468404cb17bc33bc0bf
velikile <levbo3@yahoo.com>
2017-02-15 22:00:57 +0200

    add rewind Data class stores the directions and position in a time per frame position and direction

---
+++ RewindData.java
@@ -0,0 +1,9 @@
+public class RewindData
+{
+	//store 1000 frames and let me rewind back all the way back 
+	public int FrameCount  =0;
+	public final int size = 600; // the number maximum frames stored approx 60fps
+	public int ParticlesCount = 0; // the number maximum frames stored
+	public V2[][]positions=new V2]ParticlesCount][size];
+	public V2[][]directions = new V2[size]
+}
