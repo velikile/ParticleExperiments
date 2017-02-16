93a880b2780b22edb00e1fbfaf81d26c38de0252
velikile <levbo3@yahoo.com>
2017-02-16 18:44:46 +0200

    test 01

--- RewindData.java
+++ RewindData.java
@@ -2,8 +2,21 @@ public class RewindData
 {
 	//store 1000 frames and let me rewind back all the way back 
 	public int FrameCount  =0;
-	public final int size = 600; // the number maximum frames stored approx 60fps
+	public int StoredFrameCount = 0; // the number maximum frames stored approx 60fps
 	public int ParticlesCount = 0; // the number maximum frames stored
-	public V2[][]positions=new V2]ParticlesCount][size];
-	public V2[][]directions = new V2[size]
+	public int timePerRecord= 10; // samples per second
+	public V3[][]positions=new V3[ParticlesCount][StoredFrameCount];
+	public V3[][]directions = new V3[ParticlesCount][StoredFrameCount];
+
+	//After rewind start recording from the first index 0 stop rewind when frame count=0 
+	// each 100 ms record data foreach particle test time once before every particles loop 
+	//on click on left key on arrow keys start rewind or just take the directions from the positions and directions from previous frame 
+
+	public RewindData(int ParticlesCount,int StoredFrameCount)
+	{
+		this.ParticlesCount = ParticlesCount;
+		this.StoredFrameCount = StoredFrameCount;
+	}
+	// I'll create a circuler buffer where the current positiong goes Back while the data is valid 
+	// first step just do simple substitution for direction and positions for each frame 
 }
