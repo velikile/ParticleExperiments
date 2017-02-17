3ea75d1f1e172eb56a8ec8877f646eb4707afb1d
velikile <levbo3@yahoo.com>
2017-02-17 08:12:46 +0200

    Remove Unnecessary draw call

--- RewindData.java
+++ RewindData.java
@@ -5,8 +5,8 @@ public class RewindData
 	public int StoredFrameCount = 0; // the number maximum frames stored approx 60fps
 	public int ParticlesCount = 0; // the number maximum frames stored
 	public int timePerRecord= 10; // samples per second
-	public V3[][]positions=new V3[ParticlesCount][StoredFrameCount];
-	public V3[][]directions = new V3[ParticlesCount][StoredFrameCount];
+	public V3[][]positions = null;
+	public V3[][]directions = null;
 
 	//After rewind start recording from the first index 0 stop rewind when frame count=0 
 	// each 100 ms record data foreach particle test time once before every particles loop 
@@ -16,6 +16,8 @@ public class RewindData
 	{
 		this.ParticlesCount = ParticlesCount;
 		this.StoredFrameCount = StoredFrameCount;
+		positions=new V3[ParticlesCount][StoredFrameCount];
+		directions = new V3[ParticlesCount][StoredFrameCount];
 	}
 	// I'll create a circuler buffer where the current positiong goes Back while the data is valid 
 	// first step just do simple substitution for direction and positions for each frame 
